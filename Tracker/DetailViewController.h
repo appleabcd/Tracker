8063124f2c0a201c3081be51879266e9ca9cb924
wilekson <wilekson@wileksons-macbook-pro.local>
2016-09-18 14:12:48 +0700

    Initial Commit

---
+++ Tracker/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Tracker
+//
+//  Created by wilekson on 189//16.
+//  Copyright © 2016 Arvi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ Tracker/MasterViewController.h
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.h
+//  Tracker
+//
+//  Created by wilekson on 189//16.
+//  Copyright © 2016 Arvi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
